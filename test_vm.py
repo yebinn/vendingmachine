5daee7a5a3eaa0b00b9fa787003d7d771266def5
yebinn <flugent.j@gmail.com>
2018-08-14 16:50:37 +0900

    VendingMachine 클래스 생성

--- test_vm.py
+++ test_vm.py
@@ -1,16 +1,21 @@
-from vm import run, init
+from vm import VendingMachine
 
 def test_initial_change_should_be_zero():
-    init()
-    assert "잔액은 0원입니다" == run("잔액")
+    m = VendingMachine()    #클래스()하면 instance가 생김
+    assert "잔액은 0원입니다" == m.run("잔액")
+
+
+def test_initial_change_should_be_zero():
+    m = VendingMachine()
+    assert "잔액은 0원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
-    init()
-    assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 100원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 def test_accumulation_of_change():
-    init()
-    run("동전 100")
-    run("동전 100")
-    assert "잔액은 200원입니다" == run("잔액")
+    m = VendingMachine()
+    m.run("동전 100")
+    m.run("동전 100")
+    assert "잔액은 200원입니다" == m.run("잔액")
--- vm.py
+++ vm.py
@@ -1,18 +1,16 @@
-change = 0
+class VendingMachine:
+    def __init__(self):
+        self._change = 0
 
-def init():
-    global change
-    change = 0
+    def run(self, raw):
+        tokens = raw.split(" ")
+        cmd, params = tokens[0], tokens[1:]
 
-def run(raw):
-    global change
-
-    tokens = raw.split(" ")
-    cmd, params = tokens[0], tokens[1:]
-
-    if cmd == "잔액":
-        return "잔액은 " + str(change) + "원입니다"
-    else:
-        coin = params[0]
-        change += int(coin)
-        return coin + "원을 넣었습니다"
+        if cmd == "잔액":
+            return "잔액은 " + str(self._change) + "원입니다"
+        elif cmd == "동전":
+            coin = params[0]
+            self._change += int(coin)
+            return coin + "원을 넣었습니다"
+        else:
+            return "알 수 없는 명령입니다"
