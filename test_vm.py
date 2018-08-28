fdf6d9ce617c30091e5046116d645c2294a971de
yebinn <flugent.j@gmail.com>
2018-08-28 10:39:49 +0900

    테스트 케이스 한글화

--- test_vm.py
+++ test_vm.py
@@ -1,18 +1,19 @@
 from vm import VendingMachine
 
-def test_initial_change_should_be_zero():
+def test_초기_잔액은_0원():
     m = VendingMachine()    #클래스()하면 instance가 생김
     assert "잔액은 0원입니다" == m.run("잔액")
 
-
-def test_initial_change_should_be_zero():
-    m = VendingMachine()
-    assert "잔액은 0원입니다" == m.run("잔액")
-
-def test_insert_coin_and_check():
+def test_동전_넣고_잔액_검사():
     m = VendingMachine()
     assert "100원을 넣었습니다" == m.run("동전 100")
     assert "잔액은 100원입니다" == m.run("잔액")
+
+def test_잔액_누적():
+    m = VendingMachine()
+    m.run("동전 100")
+    m.run("동전 100")
+    assert "잔액은 200원입니다" == m.run("잔액")
 
 def test_동전_인식():
     m = VendingMachine()
@@ -40,8 +41,6 @@ def test_동전이_부족한_상황에서_음료_뽑기():
     assert "잔액이 부족합니다" == m.run("음료 커피")
     assert "잔액은 100원입니다" == m.run("잔액")
 
-def test_accumulation_of_change():
-    m = VendingMachine()
-    m.run("동전 100")
-    m.run("동전 100")
-    assert "잔액은 200원입니다" == m.run("잔액")
+# def test_유효하지_않은_동전():
+#     m = VendingMachine()
+#     assert 
